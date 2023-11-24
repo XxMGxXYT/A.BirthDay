293746df66cfc6b06d68bb002560baf57b5b90ae
XxMGxX IT/AI <mohamad7719603@gmail.com>
2023-11-25 00:48:34 +0200

    Fixing some bugs

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let test = document.getElementById("test")
 let msg = document.getElementById("msg")
 let cube = document.querySelectorAll(".cube")
 let rightCube = document.querySelector(".correct")
-let cong = document.querySelectorAll(".cong")
+let cong = document.querySelector(".cong")
 
 // Check the user personality
 let correctPass = "00!000000!"
@@ -23,7 +23,9 @@ theGate()
 
 // Disappering function
 function Disappering(id){
+    // Adding "Visibility" to the element => visibility: hidden;
     id.classList.add("hide")
+    // Adding "none" to the element => display: none;
     setTimeout(() => {
         id.classList.add("none")
     }, 1000)
@@ -31,7 +33,9 @@ function Disappering(id){
 
 // Appering function
 function Appering(id){
+    // Removing "none" from the element => display: none;
     id.classList.remove("none")
+    // Removing "Visibility" from the element => visibility: hidden;
     setTimeout(() => {
         id.classList.remove("hide")
     }, 1500)
@@ -39,29 +43,45 @@ function Appering(id){
 
 // Wrong cube choosen
 cube.forEach(function(e){
+    // Adding click event for each cube
     e.addEventListener("click", () =>{
+        // if the correct cube
         if(e.classList.contains("correct")){
+            // Adding green color to the cube
             e.classList.add("greenShadow")
+            // Make the cube non clickable
             e.classList.add("none_click")
+            // Winning sound effect
             document.getElementById("win").play()
-            cong.forEach((c) =>{
-                c.classList.remove("none")
-            })
+            // Remove the display: none; from the cong.gif
+            cong.classList.remove("none")
+            // Calling Disappering function after 1 second
             setTimeout(() =>{
                 Disappering(test)
             }, 1000)
+            // Calling Appering function after 2 seconds
             setTimeout(() => {
                 Appering(msg)
             }, 2000)
+            // Removing the cong.gif after 5 seconds
+            setTimeout(() => {
+                document.getElementById("cong").remove()
+            }, 5000)
+        // if not the correct cube
         } else{
+            // Adding red color to the cube
             e.classList.add("redShadow")
+            // Removing red color of the cube after 700 mill Seconds
             setTimeout(() =>{
                 e.classList.remove("redShadow")
             }, 700)
+            // Make the cube disappering
             setTimeout(() =>{
                 e.classList.add("hide")
             }, 700)
+            // Make the cube non clickable
             e.classList.add("none_click")
+            // Wrong answer sound effect
             document.getElementById("boo").play()
         }
     })
